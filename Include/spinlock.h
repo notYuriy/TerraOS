0d318514e513b3cb49dfd521e152289d301fa308
Yuriy Zamyatin <umz38@yandex.ru>
2020-01-22 11:06:25 +0300

    removed "extern" from spinlocks

--- Include/spinlock.h
+++ Include/spinlock.h
@@ -5,7 +5,7 @@
 
 typedef uint64_t spinlock_t;
 
-extern void spinlock_lock(spinlock_t* lock);
-extern void spinlock_unlock(spinlock_t* lock);
+void spinlock_lock(spinlock_t* lock);
+void spinlock_unlock(spinlock_t* lock);
 
 #endif
